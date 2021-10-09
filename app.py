c294dba2355f4ee3bbd2f93ddf6324528e5f5855
Syed Umair Hassan Kazmi <ali_umair130@hotmail.com>
2021-10-09 08:36:11 +0500

    cleaner code

--- app.py
+++ app.py
@@ -1,56 +1,26 @@
+#Import Librarires
 from flask import Flask, render_template, request
 import tensorflow as tf
-
+import matplotlib.pyplot as plt
 import numpy as np
-import os
-import time
-import json
-import gc
-from glob import glob
 import pickle
-import pandas as pd
-
 from tensorflow import keras
-from tensorflow.keras import layers
-
 from tensorflow.keras.applications import ResNet50
-
-from tensorflow.keras.models import Model, Sequential
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.utils import plot_model
-from tensorflow.keras.layers import Input
-from tensorflow.keras.layers import Dense, BatchNormalization
-from tensorflow.keras.layers import LSTM
-from tensorflow.keras.layers import Embedding
-from tensorflow.keras.layers import Dropout
-from tensorflow.keras.layers import add
-from tensorflow.keras.callbacks import ModelCheckpoint
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.callbacks import ModelCheckpoint
-from tensorflow.keras.layers import Dense, GRU, Flatten,Input, Convolution2D, Dropout, LSTM, TimeDistributed, Embedding, Bidirectional, Activation, RepeatVector,Concatenate
-from tensorflow.keras.optimizers import RMSprop, Adam
-from tensorflow.keras.losses import SparseCategoricalCrossentropy
+from tensorflow.keras.models import Model
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
-extract_model = tf.keras.applications.ResNet50(include_top=True)
+#Import resnet model for feature extraction
+extract_model = ResNet50(include_top=True)
 last = extract_model.layers[-2].output
 image_features_extract_model = Model(inputs = extract_model.input,outputs = last)
 
-
-with open('tokenizer.pickle', 'rb') as handle:
-    tokenizer = pickle.load(handle)
-
-
-
-vocab_size = len(tokenizer.word_index) 
-max_length = 74
-max_len = max_length -1
- 
+#Import the CNN RNN model  
 reconstructed_model = load_model("model_ImageCap_Umair.h5")
 
+#Import tokenizor from pickle file
+with open('./tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
 
 
 app = Flask(__name__)
@@ -65,7 +35,7 @@ def index():
 @app.route('/after', methods=['GET', 'POST'])
 def after():
 
-    global reconstructed_model, image_features_extract_model, tokenizer, load_img, img_to_array, image
+    global reconstructed_model, image_features_extract_model, tokenizer, image
 
     file = request.files['file1']
 
@@ -74,6 +44,8 @@ def after():
     print("="*50)
     print("IMAGE SAVED")
 
+    #State vocab size, max length(max tokens)
+    vocab_size = len(tokenizer.word_index) 
     max_tokens = 73
     START_TOKEN = '<start> '
     END_TOKEN = ' <end>'
@@ -81,16 +53,21 @@ def after():
     token_end = tokenizer.word_index[END_TOKEN.strip()]
 
 
+    #Import the file, incase of an application the user will upload the file here
     image_path = 'static/file.jpg'
 
+    #Presprocess the image
     img = image.load_img(image_path, target_size=(224, 224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     image = tf.keras.applications.resnet.preprocess_input(x)
 
+    #Use the prediction coding,extarct image features from ResNet50 model and RNN features from ------>
+    #Reconstructed model, use that model to predict
     encoder_input = image_features_extract_model.predict(image)
     encoder_input = tf.reshape(encoder_input,
                                 (2048, ))
+
     encoder_input = np.expand_dims(encoder_input, axis=0)
 
     shape = (1, max_tokens)
@@ -116,8 +93,9 @@ def after():
         
         count_tokens += 1
 
+    #Print prediction    
     final = tokenizer.sequences_to_texts([output])
-
+    print(final)
     
     return render_template('after.html', final = final)
 
